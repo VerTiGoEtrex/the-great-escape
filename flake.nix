4588d6487dfb299ff81ec78811c75f6eaf21069e
Noah Crocker <2846960+VerTiGoEtrex@users.noreply.github.com>
2022-08-21 03:56:02 -0700

    setup mounts

--- flake.nix
+++ flake.nix
@@ -6,30 +6,24 @@
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let 
       pkgs = nixpkgs.legacyPackages.${system}; 
-      script = pkgs.writeShellScript "escape.sh" ''
+      setupMountsScript = pkgs.writeShellScript "escape.sh" ''
         set -euo pipefail
-        echo "hello from container"
-        TEMPDIR="$(mktemp -d)"
-        trap 'rm -rf $TEMPDIR' EXIT
-
-        mkdir -p "$TEMPDIR"/work
-        mount -t overlay -o lowerdir="$HOME",upperdir="$HOME"/i-am-homeless/ncrocker,workdir="$TEMPDIR"/work none "$HOME"
-        trap 'umount "$HOME" && rm -rf "$TEMPDIR"' EXIT
-
-        touch "$HOME/this_is_an_overlay"
+        WORKDIR=$HOME/i-am-homeless/ncrocker
+        mkdir -p $WORKDIR/sources
+        mount --bind -o rw $HOME/sources $WORKDIR/sources
+        mount --rbind -o rw $WORKDIR $HOME
+        exec unshare -U zsh
         '';
     in {
       devShells.default = pkgs.mkShell {
         name = "the-great-escape";
-        nativeBuildInputs = [ pkgs.bubblewrap ];
+        nativeBuildInputs = [ pkgs.bubblewrap pkgs.tree ];
         shellHook = ''
            set -euo pipefail
            echo "Escaping hell!"
            echo $HOME
-           mkdir -p $HOME/i-am-homeless/ncrocker
            export PATH="/run/current-system/sw/bin:$PATH"
-           #exec bwrap --bind / / --dev /dev --ro-bind $HOME $HOME --bind $HOME/sources $HOME/sources --bind $HOME/i-am-homeless $HOME/i-am-homeless bash -c "mount -t overlay overlay -o lowerdir=$HOME,upperdir=$HOME/i-am-homeless,workdir=$(mktemp -d) $(mktemp -d) && zsh"
-           exec bwrap --cap-add CAP_SETPCAP --cap-add CAP_DAC_OVERRIDE --cap-add CAP_SYS_ADMIN --bind / / --dev /dev --ro-bind $HOME $HOME --bind $HOME/sources $HOME/sources --bind $HOME/i-am-homeless $HOME/i-am-homeless -- ${script}
+           exec unshare -rm ${setupMountsScript}
         ''; 
       };
     }
