2dd221265ad96d80839f0e0fd65f32f907ea49bb
Noah Crocker <2846960+VerTiGoEtrex@users.noreply.github.com>
2022-08-21 02:18:10 -0700

    progress

--- flake.nix
+++ flake.nix
@@ -6,13 +6,31 @@
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let 
       pkgs = nixpkgs.legacyPackages.${system}; 
+      script = pkgs.writeShellScript "escape.sh" ''
+        set -euo pipefail
+        echo "hello from container"
+        TEMPDIR="$(mktemp -d)"
+        trap 'rm -rf $TEMPDIR' EXIT
+
+        mkdir -p "$TEMPDIR"/work
+        mount -t overlay -o lowerdir="$HOME",upperdir="$HOME"/i-am-homeless/ncrocker,workdir="$TEMPDIR"/work none "$HOME"
+        trap 'umount "$HOME" && rm -rf "$TEMPDIR"' EXIT
+
+        touch "$HOME/this_is_an_overlay"
+        '';
     in {
       devShells.default = pkgs.mkShell {
         name = "the-great-escape";
+        nativeBuildInputs = [ pkgs.bubblewrap ];
         shellHook = ''
-           echo "Hello from nix!"
-           exec zsh
-        '';
+           set -euo pipefail
+           echo "Escaping hell!"
+           echo $HOME
+           mkdir -p $HOME/i-am-homeless/ncrocker
+           export PATH="/run/current-system/sw/bin:$PATH"
+           #exec bwrap --bind / / --dev /dev --ro-bind $HOME $HOME --bind $HOME/sources $HOME/sources --bind $HOME/i-am-homeless $HOME/i-am-homeless bash -c "mount -t overlay overlay -o lowerdir=$HOME,upperdir=$HOME/i-am-homeless,workdir=$(mktemp -d) $(mktemp -d) && zsh"
+           exec bwrap --cap-add CAP_SETPCAP --cap-add CAP_DAC_OVERRIDE --cap-add CAP_SYS_ADMIN --bind / / --dev /dev --ro-bind $HOME $HOME --bind $HOME/sources $HOME/sources --bind $HOME/i-am-homeless $HOME/i-am-homeless -- ${script}
+        ''; 
       };
     }
   );
